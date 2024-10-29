91f475714eca98e7c47da1443895ab76cf45a293
Sithum Sandaruwan <164480083+thesithumsandaruwan@users.noreply.github.com>
2024-10-29 12:17:22 +0530

    Update TrainSchedule.java

--- TrainSchedule.java
+++ TrainSchedule.java
@@ -1,11 +1,5 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 import java.time.*;
-import java.time.format.*;
-import java.util.*;
 
-// Class to represent a Train Schedule
 class TrainSchedule {
     LocalTime departureTime;
     LocalTime arrivalTime;
